4a82899c64a6ce61261294166fc985d4df8573ae
LeeJoEun-01 <joeun1005@naver.com>
2026-01-11 14:47:31 +0900

    setting: 프로젝트 생성 및 세팅
    - PR 템플릿 및 gitignore 설정

---
+++ Sseotdabwa-iOS/Sseotdabwa-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sseotdabwa-iOS
+//
+//  Created by 이조은 on 1/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sseotdabwa-iOS/Sseotdabwa-iOS/Sseotdabwa_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sseotdabwa_iOSApp.swift
+//  Sseotdabwa-iOS
+//
+//  Created by 이조은 on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Sseotdabwa_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
