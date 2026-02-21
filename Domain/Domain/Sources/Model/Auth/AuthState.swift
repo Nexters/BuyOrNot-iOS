756f7c940482a0df1ec43aa4c19e9241bf3c386e
moonjs0113 <littlemoom@naver.com>
2026-02-21 20:59:07 +0900

    [#49] feat: 스플래시 > 로그인/메인, 로그인 > 메인 전환

--- Domain/Domain/Sources/Model/Auth/AuthState.swift
+++ Domain/Domain/Sources/Model/Auth/AuthState.swift
@@ -1,11 +1,11 @@
 //
-//  LoginResult.swift
+//  AuthState.swift
 //  Auth
 //
 //  Created by 문종식 on 2/21/26.
 //
 
-public enum LoginResult {
+public enum AuthState {
     case member
     case guest
 }
