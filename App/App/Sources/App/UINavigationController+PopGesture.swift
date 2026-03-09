900932cd6fcf40b592cfbd2ee817f380c5cd2754
LeeJoEun-01 <joeun1005@naver.com>
2026-03-09 16:20:18 +0900

    [#67] feat: 뒤로 가기 스와이프 구현

---
+++ App/App/Sources/App/UINavigationController+PopGesture.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+PopGesture.swift
+//  App
+//
+//  Created by 이조은 on 3/9/26.
+//
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
