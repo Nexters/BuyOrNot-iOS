9466102960478f463739e08a8bbde6d0cce3366e
moonjs0113 <littlemoom@naver.com>
2026-03-15 15:46:52 +0900

    [#64] fix: FeedCategory 불필요한 초기화 함수 제거

--- Domain/Domain/Sources/Model/Feed/FeedCategory.swift
+++ Domain/Domain/Sources/Model/Feed/FeedCategory.swift
@@ -15,31 +15,6 @@ public enum FeedCategory: String, CaseIterable {
     case health = "HEALTH"
     case book = "BOOK"
     case etc = "ETC"
-    
-    public init(rawValue: String) {
-        switch rawValue {
-        case "LUXURY":
-            self = .luxury
-        case "FASHION":
-            self = .fashion
-        case "BEAUTY":
-            self = .beauty
-        case "FOOD":
-            self = .food
-        case "ELECTRONICS":
-            self = .electronics
-        case "TRAVEL":
-            self = .travel
-        case "HEALTH":
-            self = .health
-        case "BOOK":
-            self = .book
-        case "ETC":
-            self = .etc
-        default:
-            self = .etc
-        }
-    }
 
     public var displayName: String {
         switch self {
