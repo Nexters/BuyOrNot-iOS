bf78bb11940f93fe9e75e7bc64d80ca2227daa87
moonjs0113 <littlemoom@naver.com>
2026-03-23 20:50:09 +0900

    [#73] refact: ColorPalette 추가

---
+++ DesignSystem/DesignSystem/Sources/Color/ColorPalette.swift
@@ -0,0 +1,31 @@
+//
+//  ColorPalette.swift
+//  DesignSystem
+//
+//  Created by 문종식 on 3/23/26.
+//
+
+import SwiftUI
+
+enum ColorPalette {
+    public static let black = Color("black", bundle: .module)
+    public static let google = Color("google", bundle: .module)
+    public static let apple = Color("apple", bundle: .module)
+    public static let kakao = Color("kakao", bundle: .module)
+    public static let gray0 = Color("gray0", bundle: .module)
+    public static let gray50 = Color("gray50", bundle: .module)
+    public static let gray100 = Color("gray100", bundle: .module)
+    public static let gray200 = Color("gray200", bundle: .module)
+    public static let gray300 = Color("gray300", bundle: .module)
+    public static let gray400 = Color("gray400", bundle: .module)
+    public static let gray500 = Color("gray500", bundle: .module)
+    public static let gray600 = Color("gray600", bundle: .module)
+    public static let gray700 = Color("gray700", bundle: .module)
+    public static let gray800 = Color("gray800", bundle: .module)
+    public static let gray900 = Color("gray900", bundle: .module)
+    public static let gray1000 = Color("gray1000", bundle: .module)
+    public static let green100 = Color("green100", bundle: .module)
+    public static let green200 = Color("green200", bundle: .module)
+    public static let red100 = Color("red100", bundle: .module)
+    public static let blue100 = Color("blue100", bundle: .module)
+}
