1e1c8e944e91cc43f0e71b92f97f433b6f95f77d
moonjs0113 <littlemoom@naver.com>
2026-03-29 17:44:28 +0900

    [#87] fix: 로그아웃 상태 감지 강화

---
+++ Core/Core/Sources/Extensions/Notification+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Extension.swift
+//  Core
+//
+//  Created by 문종식 on 3/29/26.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    static let authSessionDidExpire = Notification.Name("authSessionDidExpire")
+}
