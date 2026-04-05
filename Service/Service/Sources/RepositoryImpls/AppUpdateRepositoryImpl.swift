2b02863ca375e18b941f4b577b46b500427ac034
moonjs0113 <littlemoom@naver.com>
2026-04-05 15:39:27 +0900

    [#92] feat: AppUpdateRepository 추가

--- Service/Service/Sources/RepositoryImpls/AppUpdateRepositoryImpl.swift
+++ Service/Service/Sources/RepositoryImpls/AppUpdateRepositoryImpl.swift
@@ -7,43 +7,28 @@
 
 import Domain
 import Foundation
-import FirebaseRemoteConfig
 
 public final class AppUpdateRepositoryImpl: AppUpdateRepository {
-    private let remoteConfig: RemoteConfig
     private let store: AppUpdateStore
 
-    public init(remoteConfig: RemoteConfig = .remoteConfig()) {
-        self.remoteConfig = remoteConfig
+    public init() {
         self.store = AppUpdateStore()
     }
 
     public func fetchAndActivate() async throws {
-        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
-            remoteConfig.fetchAndActivate { _, error in
-                if let error {
-                    continuation.resume(throwing: error)
-                    return
-                }
-                continuation.resume(returning: ())
-            }
-        }
+        try await store.fetchAndActivate()
     }
 
     public func getAppUpdateInfo() async -> AppUpdateInfo {
         try? await fetchAndActivate()
-        let latestVersion = AppVersion(
-            rawValue: remoteConfig.configValue(forKey: "ios_latest_version").stringValue
-        )
-        let minimumVersion = AppVersion(
-            rawValue: remoteConfig.configValue(forKey: "ios_minimum_version").stringValue
-        )
-        let strategyString = remoteConfig.configValue(forKey: "ios_update_strategy").stringValue
+        let latestVersion = store.getLatestVersion()
+        let minimumVersion = store.getMinimumVersion()
+        let strategyString = store.getUpdateStrategyRawValue()
 
         return AppUpdateInfo(
             latestVersion: latestVersion,
             minimumVersion: minimumVersion,
-            updateStrategy: Self.parseUpdateStrategy(strategyString)
+            updateStrategy: UpdateStrategy(remoteValue: strategyString)
         )
     }
 
@@ -56,16 +41,5 @@ public final class AppUpdateRepositoryImpl: AppUpdateRepository {
 
     public func markSoftUpdateShown() {
         store.saveLastSoftUpdateShownAt(Date())
-    }
-
-    private static func parseUpdateStrategy(_ strategy: String) -> UpdateStrategy {
-        switch strategy.trimmingCharacters(in: .whitespacesAndNewlines).uppercased() {
-        case "FORCE":
-            return .force
-        case "SOFT":
-            return .soft
-        default:
-            return .none
-        }
     }
 }
